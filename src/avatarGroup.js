3cb6f582f288ddc8bf3d3ff706cd967a102baf21
luciana.lucio <luciana.lucio@g.globo>
2024-03-27 18:01:51 -0300

    feat: create GroupAvatars component

--- src/avatarGroup.js
+++ src/avatarGroup.js
@@ -1,1 +1,23 @@
-// nome do componente Avatargroup
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+import AvatarGroup from '@material-ui/lab/AvatarGroup';
+
+export default function GroupAvatars() {
+  return (
+    <Grid container direction="row">
+      <Grid item>
+
+      </Grid>
+      <Grid item>
+        <AvatarGroup max={3}>
+          <Avatar alt="Remy Sharp" src="" />
+          <Avatar alt="Travis Howard" src="" />
+          <Avatar alt="Cindy Baker" src="" />
+          <Avatar alt="Agnes Walker" src="" />
+          <Avatar alt="Trevor Henderson" src="" />
+        </AvatarGroup>
+      </Grid>
+    </Grid>
+  );
+}
