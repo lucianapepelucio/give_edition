fa8758b61153d456d5c046662f91da7458c6d5c3
luciana.lucio <luciana.lucio@g.globo>
2024-03-28 19:00:31 -0300

    style: add style in GroupAvatars component

--- src/avatarGroup.js
+++ src/avatarGroup.js
@@ -1,23 +1,63 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Grid from '@material-ui/core/Grid';
+import { Avatar, Badge, Grid, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import AvatarGroup from '@material-ui/lab/AvatarGroup';
+import CreateIcon from '@material-ui/icons/Create';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    left: theme.spacing(2),
+  },
+  paper: {
+    padding: theme.spacing(1),
+    border: "1px solid #E0E0E0",
+    display: 'flex',
+  },
+  icon: {
+    color: "#138848",
+  },
+  editorAvatar: {
+    margin: theme.spacing(1),
+    border: '3px solid #138848',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+  },
+}));
 
 export default function GroupAvatars() {
-  return (
-    <Grid container direction="row">
-      <Grid item>
+  const classes = useStyles();
 
-      </Grid>
-      <Grid item>
-        <AvatarGroup max={3}>
-          <Avatar alt="Remy Sharp" src="" />
-          <Avatar alt="Travis Howard" src="" />
-          <Avatar alt="Cindy Baker" src="" />
-          <Avatar alt="Agnes Walker" src="" />
-          <Avatar alt="Trevor Henderson" src="" />
-        </AvatarGroup>
-      </Grid>
+  return (
+    <Grid container className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid container direction="row">
+          <Grid item xs={1} sm={2} md={3}>
+            <Badge
+              overlap="circle"
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              badgeContent={<CreateIcon className={classes.icon} />}
+            >
+              <Avatar alt="editor" src="" className={classes.editorAvatar} />
+            </Badge>
+          </Grid>
+          
+          <Grid item xs={1} sm={2} md={3} className={classes.avatar}>
+            <AvatarGroup max={3}>
+              <Avatar alt="user.name" src="" />
+              <Avatar alt="user.name" src="" />
+              <Avatar alt="user.name" src="" />
+              <Avatar alt="user.name" src="" />
+              <Avatar alt="user.name" src="" />
+            </AvatarGroup>
+          </Grid>
+        </Grid>
+      </Paper>
     </Grid>
   );
 }
